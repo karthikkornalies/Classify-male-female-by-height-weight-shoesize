e903cc09a99eb7966159b787911c7e863b96f7e7
baohnguyen <baohnguyen95@gmail.com>
2018-03-01 20:24:58 -0800

    modified demo.py, in this modified version, i use three method tree, nearest neighbors and random forest

--- demo.py
+++ demo.py
@@ -1,16 +1,32 @@
-from  sklearn import tree
+#import enssential dependencies from sklearn
+from  sklearn import neighbors
+from sklearn import tree
+from sklearn import ensemble
 
 #[height, weight, shoe size]
 X = [[181,80,44], [177, 70, 43], [160, 60, 38], [154, 54,37], 
 	 [166,65,40], [190,90,47], [175,64,39], [177,70,40], [159, 55,39], [171,75,42], [181,85,43]]
 
+#gender
 Y =  ['male', 'female', 'female', 'female', 'male', 'male',
 	  'male', 'female','male', 'female', 'male']
 
-clf = tree.DecisionTreeClassifier()
+#create variables with classifiers
+tree = tree.DecisionTreeClassifier()
+neighbors = neighbors.KNeighborsClassifier()
+randomForest = ensemble.RandomForestClassifier()
 
-clf = clf.fit(X,Y)
+#fit them
+tree = tree.fit(X,Y)
+neighbors = neighbors.fit(X,Y)
+randomForest = randomForest.fit(X,Y)
 
-prediction = clf.predict([[190,70,43]])
+#create prediction variables for results
+predictionForTree = tree.predict([[167,63,41]])
+predictionForNeighbors = neighbors.predict([[167,63,41]])
+predictionForRandomForest = randomForest.predict([[167,63,41]])
 
-print(prediction)
+#print all the results
+print("DecisionTree: ", predictionForTree)
+print("NearestNeighbors: ", predictionForNeighbors)
+print("RandomForest: ", predictionForRandomForest)
